6a913146b625714927066ea4378b6475b526871e
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-03-20 21:13:35 -0500

    Background Image, Happy Birthday Greeting Text

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GreetingWithImage(message = "Happy", kind = "Birthday", name = "Sam!", from = "Basavaprasad")
                 }
             }
         }
@@ -33,16 +38,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Blue) {
-        Text(text = "Hi, My name is $name!")
+fun GreetingWithImage(message: String, kind: String, name: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null, modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            , contentScale = ContentScale.Crop
+        )
+        TypeOfGreetingWithText(message = message, kind = kind, name = name, from = from)
+    }
+}
+
+
+@Composable
+fun TypeOfGreetingWithText  (message: String, kind: String, name: String, from: String) {
+    Column {
+        Text(text = message + "..............<=>",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(text = kind + " " + name,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 90.dp, top = 16.dp)
+            )
+        Text(text = "from " + from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 150.dp, top = 16.dp)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GreetingWithImagePreview() {
     GreetingCardTheme {
-        Greeting("Basavaprasad")
+        GreetingWithImage(message = stringResource(R.string.Happy), kind = stringResource(R.string.Birthday), name = stringResource(
+                    R.string.Sam), from = stringResource(R.string.Basavaprasad)
+                            )
     }
 }
